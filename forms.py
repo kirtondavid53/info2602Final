ecd40deac339b815e9ea59519e19958fc8e03917
David Kirton <kirtondavid53@gmail.com>
2020-05-28 08:11:50 -0500

    new commit

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SubmitField
-from wtforms.validators import InputRequired, EqualTo, Email
-
-class LogIn(FlaskForm):
-  username = StringField('username', validators=[InputRequired()])
-  password = PasswordField('New Password', validators=[InputRequired()])
-  submit = SubmitField('Login', render_kw={'class': 'btn waves-effect waves-light white-text'})
